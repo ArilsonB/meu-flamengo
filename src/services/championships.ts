6458f0cc83a1f4cb19d30cdd45dd56da26564bd6
Arilson B. Ferreira <arilsonbrj@gmail.com>
2021-07-05 11:03:38 -0300

    Championship service

---
+++ src/services/championships.ts
@@ -0,0 +1,16 @@
+const championships = [
+  {
+    id: "1",
+    type: "league",
+    name: "Campeonato Brasileiro",
+    shortName: "Brasileirão",
+    divisionName: "Serie A",
+    totalClubs: 20,
+    table: [
+      {
+        pos: "1",
+        clubId: "1"
+      }
+    ]
+  },
+]
--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -1,3 +1,19 @@
 async function listVideos() {
-  await fetch(`https://api.youtube.com`)
+  const videos = await fetch(`https://api.youtube.com`).then((e) => {
+    return e;
+  }).then((err) => {
+    return err;
+  }).finally(() => {
+
+  })
+
+  try {
+    const videos = await fetch(`https://api.youtube.com`);
+  } catch {
+
+  } finally {
+
+  }
+
+  return videos;
 }
